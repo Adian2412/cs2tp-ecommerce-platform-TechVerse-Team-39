36df9f25c7cbd75fa5b8445441f3ee954bb42d38
soabirayussif-prog <soabirayussif@gmail.com>
2025-12-05 03:40:46 +0000

    Create 0001_01_01_000009_create_stock_movements_table.php

---
+++ backend/backend/database/migrations/0001_01_01_000009_create_stock_movements_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('stock_movements', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_variant_id')->constrained('product_variants')->cascadeOnDelete();
+            $table->enum('movement_type', ['IN', 'OUT', 'ADJUST']);
+            $table->integer('quantity');
+            $table->foreignId('created_by')->constrained('users')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('stock_movements');
+    }
+};
