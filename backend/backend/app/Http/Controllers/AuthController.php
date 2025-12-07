a2c967ccc088065378344b5b9be30a6a1e69f17f
soabirayussif-prog <soabirayussif@gmail.com>
2025-12-07 22:13:15 +0000

    Update AuthController.php

--- backend/backend/app/Http/Controllers/AuthController.php
+++ backend/backend/app/Http/Controllers/AuthController.php
@@ -1,17 +1,17 @@
-<?php
+//<?php
 
-namespace App\Http\Controllers;
+//namespace App\Http\Controllers;
 
-use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\ValidationException;
+//use App\Models\User;
+//use Illuminate\Http\Request;
+//use Illuminate\Support\Facades\Hash;
+//use Illuminate\Validation\ValidationException;
 
-class AuthController extends Controller
-{
+//class AuthController extends Controller
+//{
    
-    public function register(Request $request)
-    {
+    //public function register(Request $request)
+    //{
         // add validate input (name, email, password)
         // $data = $request->validate([...]);
 
@@ -19,29 +19,29 @@ class AuthController extends Controller
         // $user = User::create([...]);
 
         // add  issue token (Sanctum) and return
-        return response()->json(['message' => 'register endpoint ready'], 201);
-    }
+       // return response()->json(['message' => 'register endpoint ready'], 201);
+   // }
 
    
-    public function login(Request $request)
-    {
+    //public function login(Request $request)
+    //{
         // add: validate credentials
         // $user = User::where('email', $request->email)->first();
         // if (!Hash::check($request->password, $user->password_hash)) throw ValidationException...
         // create token and return
-        return response()->json(['message' => 'login endpoint ready']);
-    }
+        //return response()->json(['message' => 'login endpoint ready']);
+    //}
 
     
-    public function logout(Request $request)
-    {
+    //public function logout(Request $request)
+    //{
         // add: revoke token
-        return response()->json(['message' => 'logged out']);
-    }
+        //return response()->json(['message' => 'logged out']);
+    //}
 
     
-    public function me(Request $request)
-    {
-        return response()->json($request->user());
-    }
-}
+    //public function me(Request $request)
+    //{
+        //return response()->json($request->user());
+    //}
+//}
